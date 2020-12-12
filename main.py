7f9e2130ccd11744f86095948eb6ce2e70d216c7
GloryShovel <sk.kamil6@wp.pl>
2020-12-12 16:33:13 +0100

    CLI almost done (fix the bytes converion to be able to decrypt message)

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ DES -   First text is separated in 64 bit block (for making it easier for machin
 
 __author__ = "Kamil Skrzypkowski, Andrzej Mrozik"
 
-from Crypto.Cipher import AES, Blowfish, DES
+from Crypto.Cipher import AES, Blowfish
 from struct import pack
-# from des import DesKey
+from des import DesKey
 
 # pip install pycryptodome
 # pip install des
@@ -31,19 +31,19 @@ from struct import pack
 def AES_encode(key, message):
     """
     Encrypting function for AES algorithm \n
-    INPUT - key, message to encrypt \n
-    RETURN - encrypted message
+    INPUT - <bytearray> key, <bytes> message to encrypt \n
+    RETURN - nonce, encrypted <bytes> message
     """
 
     cipher = AES.new(key, AES.MODE_EAX)
 
     nonce = cipher.nonce
 
-    # String to bytearray required by encrypt function
-    msg = bytearray()
-    msg.extend(map(ord, message))
+    # Bytes to bytearray required by encrypt function
+    # msg = bytearray()
+    # msg.extend(map(ord, message))
 
-    ciphertext, tag = cipher.encrypt_and_digest(msg)
+    ciphertext, tag = cipher.encrypt_and_digest(message)
 
     return nonce, ciphertext
 
@@ -51,8 +51,8 @@ def AES_encode(key, message):
 def AES_decode(key, nonce, ciphertext):
     """
     Decrypting function for AES algorithm \n
-    INPUT - key, encrypted message \n
-    RETURN - decrypted message
+    INPUT - <bytearray> key, nonce, <bytes> message to decrypt \n
+    RETURN - decrypted <bytes> message
     """
 
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
@@ -64,8 +64,8 @@ def AES_decode(key, nonce, ciphertext):
 def Bf_encode(key, message):
     """
     Encrypting function for Blowfish algorithm \n
-    INPUT - key, message to encrypt \n
-    RETURN - encrypted message
+    INPUT - <bytearray> key, <bytes> message to encrypt \n
+    RETURN - encrypted <bytes> message
     """
 
     bs = Blowfish.block_size
@@ -80,8 +80,8 @@ def Bf_encode(key, message):
 def Bf_decode(key, ciphertext):
     """
     Decrypting function for Blowfish algorithm \n
-    INPUT - key, encrypted message \n
-    RETURN - decrypted message
+    INPUT - <bytearray> key, <bytes> message to decrypt \n
+    RETURN - decrypted <bytes> message
     """
 
     bs = Blowfish.block_size
@@ -99,80 +99,154 @@ def Bf_decode(key, ciphertext):
 def DES_encode(key, message):
     """
     Encrypting function for DES algorithm \n
-    INPUT -  key, message to encrypt \n
-    RETURN - encrypted message
+    INPUT -  <bytearray> key, <bytes> message to encrypt \n
+    RETURN - encrypted <bytes> message
     """
 
-    # OLD
-    # key0 = DesKey(key)
-    # ciphertext = key0.encrypt(message, padding=True)
-
-    cipher = DES.new(key, DES.MODE_OFB)
-    ciphertext = cipher.iv + cipher.encrypt(message)
+    key0 = DesKey(key)
+    ciphertext = key0.encrypt(message, padding=True)
     return ciphertext
 
 
 def DES_decode(key, ciphertext):
     """
     Decrypting function for DES algorithm \n
-    INPUT - key, encrypted message \n
-    RETURN - decrypted message
+    INPUT - <bytearray> key, <bytes> message to decrypt \n
+    RETURN - decrypted <bytes> message
     """
 
-    # OLD
-    # key0 = DesKey(key)
-    # msg = key0.decrypt(ciphertext, padding=True)
-
-    cipher = DES.new(key, DES.MODE_OFB)
-    msg = cipher.iv + cipher.decrypt(ciphertext)
+    key0 = DesKey(key)
+    msg = key0.decrypt(ciphertext, padding=True)
     return msg
 
 
+def ask_for_data(key_bit_size=8):
+    """
+    Gathers data from user and pass them to factory \n
+    INPUT - <int> mode (0 - code, 1 - decode), <int> (default = 8) encoding function key bit size\n
+    RETURN - <bytearray> key and <bytes> message
+    """
+    while True:
+        print("Tell me the ", key_bit_size, "-bit key")
+        key = str(input(">"))
+        if len(key) < key_bit_size:
+            print("The key is to short")
+            print()
+
+        elif len(key) > key_bit_size:
+            print("The key is too long")
+            print()
+
+        else:
+            break
+
+    while True:
+        print("Tell me your message")
+        message = str(input(">"))
+
+        if len(message) < 1:
+            print("There is no message to encrypt")
+            print()
+        else:
+            break
+
+    key, message = key_n_message_factory(key, message)
+    return key, message
+
+
+def key_n_message_factory(key, message):
+    """
+    Factory that takes key and message and converts them into <byte> and <bytearray> \n
+    INPUTS - <str> key, <str> message, <int> mode (0 - code, 1 - decode)
+    RETURNS - <bytearray> key, <bytes> message
+    """
+    key_bytearray = bytearray()
+    key_bytearray.extend(map(ord, key))
+
+    # TODO: make something with this shit ! (wrong byte converion when \ is in text)
+    # message_bytearray = bytearray()
+    # message_bytearray.extend(map(ord, message))
+
+    # if mode == 0:
+    message_bytes = bytes(message, 'utf-8')
+    # else:
+    #     message_bytes = message
+
+    return key_bytearray, message_bytes
+
+
+def message_back_to_string(message):
+    """
+    Factory that converts message back to strings \n
+    INPUTS - <bytes> message
+    RETURNS - <str> message
+    """
+    message = str(message)
+    message = message[2:-1]
+
+    return message
+
+
 if __name__ == '__main__':
-    # def pad(text):
-    #     n = len(text) % 8
-    #     return text + (b' ' * n)
-    #
-    #
-    # key = b'hello123'
-    # text1 = b'Python is the Best Language!'
-    #
-    # des = DES.new(key, DES.MODE_ECB)
-    #
-    # padded_text = pad(text1)
-    # encrypted_text = des.encrypt(padded_text)
-    #
-    # print(encrypted_text)
-    # print(des.decrypt(encrypted_text))
 
-    key = b'klucz123'
-    message = b'BSI to fajny przedmiot'
+    while True:
+        print("What cipher function you want to use? \n"
+            "1.DES \n"
+            "2.Blowfish \n"
+            "3.AES \n"
+            "0.Exit")
+        console_in = input(">")
 
-    # DES
-    szyfr_des = DES_encode(key, message)
-    print(szyfr_des)
-    message = DES_decode(key, szyfr_des)
-    print(message)
+        # TODO popwiedz czy chcemy tak, czy chcemy tylko inty
+        if console_in == "1" or console_in == "DES" or console_in == "des":
 
-    # BlowFish
-    szyfr_bf = Bf_encode(key, message)
-    print(szyfr_bf)
-    message = Bf_decode(key, szyfr_bf)
-    print(message)
+            # TODO: copy this input, option and if for each crypto algorithm
+            print("Chcesz kodowac czy dekodowac?")
+            option = input(">")
 
-    # AES
-    key = b"16 bytes key aes"
-    nonce, szyfr_aes = AES_encode(key, message)
-    print(szyfr_aes)
-    message = AES_decode(key, nonce, szyfr_aes)
-    print(message)
+            # DES
+            if option == "kodowac"  or option == "k":
+                key, message = ask_for_data()
+                print(message_back_to_string(message))
+                szyfr_des = DES_encode(key, message)
+                print(szyfr_des)
+            elif option == "dokodowac" or option == "d":
+                key, message = ask_for_data()
+                print(message)
+                message = DES_decode(key, message)
+                print(message_back_to_string(message))
+
+            print()
+
+        elif console_in == "2" or console_in == "BlowFish" or console_in == "Blowfish" or console_in == "blowfish":
+            key, message = ask_for_data()
+
+            # BlowFish
+            szyfr_bf = Bf_encode(key, message)
+            print(szyfr_bf)
+            message = Bf_decode(key, szyfr_bf)
+
+            message = message_back_to_string(message)
+            print(message)
+            print()
+
+        elif console_in == "3" or console_in == "AES" or console_in == "aes":
+            key, message = ask_for_data(16)
+
+            # AES
+            nonce, szyfr_aes = AES_encode(key, message)
+            print(szyfr_aes)
+            message = AES_decode(key, nonce, szyfr_aes)
+
+            message = message_back_to_string(message)
+            print(message)
+            print()
+
+        elif console_in == "0" or console_in == "Exit" or console_in == "exit" or console_in == "EXIT" or console_in == "e":
+            break
 
 #     TODO:
-#        - try again to use DES from pycryptodome module
-#         CLI
 #         handle exceptions about key length basically everywhere
-#        + documentation
 #         make it OO, SOLID and DRY
-#        + header with authors and quick documentation of algorithms
-#         sources of cipher modules (a czy my tego nie mamy?)
+#         sources of cipher modules
 
