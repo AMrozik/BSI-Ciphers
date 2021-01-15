6b26fba83e9fd91fc025595b4b6e3404b9f86b8a
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-01-15 16:44:24 +0100

    sigINT to terminate server process

--- Server-Client/client.py
+++ Server-Client/client.py
@@ -1,7 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('0.0.0.0', 8080))
+client.connect(('185.18.142.14', 8080))
 
 client.send(b"I am Client\n")
 
--- Server-Client/server.py
+++ Server-Client/server.py
@@ -1,6 +1,12 @@
 import socket
+import signal, os
 
 # https://docs.python.org/3/howto/sockets.html
+
+def handler(signum, frame):
+    print("Shutting down")
+    exit(0)
+
 
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,11 +16,12 @@ serversocket.bind(('0.0.0.0', 8080))
 serversocket.listen(5)
 
 while True:
+    signal.signal(signal.SIGINT, handler)
     connection, addr = serversocket.accept()
 
     while True:
         data = connection.recv(4096)
-        if not data: break;
+        if not data: break
         client_msg = str(data)
         print(client_msg)
 
