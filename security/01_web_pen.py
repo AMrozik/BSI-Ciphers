3be5df337cd86ca35e1157fa7589368897ee0ecd
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-01-30 14:30:27 +0100

    meh

---
+++ security/01_web_pen.py
@@ -0,0 +1,14 @@
+"""
+Modified example coming from:
+Python Web Penetration Testing Cookbook.pdf
+
+"""
+
+import requests
+
+verbs = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'TEST']
+for verb in verbs:
+    req = requests.request(verb, 'http://127.0.0.1')
+    print(verb, req.status_code, req.reason)
+    if verb == 'TRACE' and 'TRACE / HTTP/1.1' in req.text:
+        print('Possible Cross Site Tracing vulnerability found')
